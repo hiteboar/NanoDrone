a38dcbfb351abd0d215c03fd2b99647ee6337d6f
VBoschMatas <vboschmatas@gmail.com>
2024-08-15 14:40:22 +0200

    Added Unity project

---
+++ NanoDroneApp/Assets/BluetoothController.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class BluetoothController : MonoBehaviour
+{
+    public event EventHandler OnScanEnds;
+    public event EventHandler OnConnected;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Scan(){
+
+    }
+}
